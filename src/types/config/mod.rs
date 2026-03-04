01bc7bd31557e8d4535b286086a87f12b2d14f8f
RhizoNymph <quantnymph@gmail.com>
2026-03-04 13:04:08 -0800

    defaults types

--- src/types/config/mod.rs
+++ src/types/config/mod.rs
@@ -1,8 +1,11 @@
 pub mod chain;
 pub mod contract;
+pub mod defaults;
 pub mod eth_call;
 pub mod indexer;
 pub mod metrics;
 pub mod raw_data;
 pub mod tokens;
 pub mod transformations;
+// Re-export defaults module for convenience
+pub use defaults as config_defaults;
